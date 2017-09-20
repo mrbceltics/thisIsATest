cb95188a529d31e3d22b7034c80e0eb8e3a80ef4
mrbceltics <mrbceltics@gmail.com>
2017-09-20 15:35:28 +0300

    shelve

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("This is a test!!!");
+        System.out.println("after shelve");
     }
 }
